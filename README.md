4993c8d2232483ad3419afa9e0cb6f60293b5499
woohhan <woohyung_han@tmax.co.kr>
2020-02-15 18:32:43 +0900

    Add CI on github action

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# containerized-data-importer Helm Chart
+# containerized-data-importer Helm Chart ![cdi-chart](https://github.com/woohhan/cdi-chart/workflows/cdi-chart/badge.svg)
 A Helm chart for [kubevirt/containerized-data-importer](https://github.com/kubevirt/containerized-data-importer)
 
 ## Quick Start
 1. Install Helm(>3.0): [Installing Helm](https://helm.sh/docs/intro/install/)
 2. Check `values.yaml` file
-3. Install with `helm install cdi .`
-4. Uninstall with `helm uninstall cdi`
+3. Install with: `helm install cdi .`
+4. Uninstall with: `helm uninstall cdi`
--- hack.sh
+++ hack.sh
@@ -1,9 +1,37 @@
+#!/bin/bash
+set -euo pipefail
+shopt -s inherit_errexit
+
+function install {
+  helm install cdi .
+  sleep 30
+  echo 'Waiting for deployment...'
+  kubectl wait --for=condition=available deployment cdi-apiserver --timeout=30s -n cdi
+  kubectl wait --for=condition=available deployment cdi-deployment --timeout=30s -n cdi
+  kubectl wait --for=condition=available deployment cdi-operator --timeout=30s -n cdi
+  kubectl wait --for=condition=available deployment cdi-uploadproxy --timeout=30s -n cdi
+  kubectl get pods -A
+}
+
+function uninstall {
+  helm uninstall cdi
+  if [[ $(kubectl get ns | grep cdi) ]]; then sleep 10; echo 'Waiting for deleting namespace...'; fi
+  if [[ $(kubectl get ns | grep cdi) ]]; then sleep 10; echo 'Waiting for deleting namespace...'; fi
+  if [[ $(kubectl get ns | grep cdi) ]]; then sleep 10; echo 'Waiting for deleting namespace...'; fi
+  if [[ $(kubectl get ns | grep cdi) ]]; then sleep 10; echo 'Waiting for deleting namespace...'; fi
+  if [[ $(kubectl get ns | grep cdi) ]]; then sleep 10; echo 'Waiting for deleting namespace...'; fi
+  if [[ $(kubectl get ns | grep cdi) ]]; then sleep 10; echo 'Waiting for deleting namespace...'; fi
+  if [[ $(kubectl get ns | grep cdi) ]]; then false; fi
+  kubectl get pods -A
+  kubectl get ns
+}
+
 case "${1:-}" in
 i)
-  helm install cdi .
+  install
   ;;
 u)
-  helm uninstall cdi
+  uninstall
   ;;
 t)
   helm template .
